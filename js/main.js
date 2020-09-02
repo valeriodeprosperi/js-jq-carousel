15c74067fa21fb8d273023c9f5766a96077352f7
valerio <valerio.deprosperi@gmail.com>
2020-09-02 15:43:02 +0200

    function next

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  // function di next
+  $("next").click(nextImg);
+
+  function nextImg(){
+    var imgActive = $("img.active");
+    imgActive.removeClass("active");
+
+    if(imgActive.hasClass("last") == true) {
+      var nextImg = $("img.first");
+    } else {
+      var nextImg = imgActive.next();
+    }
+
+    nextImg.addClass("active");
+  }
+});
